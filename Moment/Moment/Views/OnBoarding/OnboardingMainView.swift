43e60b221ee4e3f600f635588d9862043d74d07a
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-03-28 23:50:41 +0900

    [Modify] store연결

--- Moment/Moment/Views/OnBoarding/OnboardingMainView.swift
+++ Moment/Moment/Views/OnBoarding/OnboardingMainView.swift
@@ -6,33 +6,66 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+// MARK: - Onboarding DATA MODEL
+struct OnboardingData: Identifiable, Equatable {
+	var id = UUID()
+	var title: String
+	var image: String
+}
 
 struct OnboardingMainView: View {
-    var onboardings: [OnboardingData] = OnboardingData.onboardingsData
+	let store: StoreOf<OnBoardingFeature>
     
     var body: some View {
         VStack {
             TabView {
-                ForEach(onboardings[0...2]) { item in
+				ForEach(store.onboardings) { item in
                     OnboardingCardView(onboarding: item)
                 }
             }
             .tabViewStyle(PageTabViewStyle())
             .padding(.vertical, 20)
             .onAppear {
-                setupAppearance()
+				store.send(.setupAppearance)
             }
-            StartButtonView()
+			
+			Button {
+				store.send(.completeOnboarding)
+			} label: {
+				HStack(spacing: 8) {
+					Text("시작하기")
+						.foregroundStyle(Color.white)
+				}
+				.frame(width: 240, height: 50)
+				.background(.mainBrown)
+				.cornerRadius(24)
+			}
         }
-
     }
-    
-    func setupAppearance() {
-        UIPageControl.appearance().currentPageIndicatorTintColor = .mainBrown
-        UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.2)
-      }
+}
+
+private struct OnboardingCardView: View {
+	var onboarding: OnboardingData
+	
+	var body: some View {
+		VStack(alignment: .center) {
+			Text(onboarding.title)
+				.font(.title)
+				.foregroundStyle(Color.mainBrown)
+			
+			Image(onboarding.image)
+				.resizable()
+				.frame(width: 296, height: 449)
+				.padding()
+		}
+	}
 }
 
 #Preview {
-    OnboardingMainView()
+	OnboardingMainView(
+		store: Store(initialState: OnBoardingFeature.State()) {
+			OnBoardingFeature()
+		})
 }
