06a1e530374507c5b7c1da81e93b9e543eca1378
Phangg <leeckdwns9305@gmail.com>
2024-04-08 18:01:59 +0900

    [Add] AppStartView TDD 작성
    Splash / Onboarding 관련
    TDD 작성

--- Moment_TCA_1.9.2/Moment/MomentTests/AppStartTests.swift
+++ Moment_TCA_1.9.2/Moment/MomentTests/AppStartTests.swift
@@ -12,6 +12,50 @@ import ComposableArchitecture
 @testable import Moment
 
 // MARK: - Splash / Onboarding 화면 테스트
+@MainActor
 final class AppStartTests: XCTestCase {
+    
+    // splash 화면 테스트
+    func appLogoTest() async {
+        var isOnboardingCompleted: Bool = false
+        let commons = Commons(isCompleteOnboarding: { isOnboardingCompleted },
+                              completeOnboarding: { isOnboardingCompleted = true })
+        
+        let store = TestStore(initialState: AppStartFeature.State()) {
+            AppStartFeature()
+        } withDependencies: {
+            $0.commons = commons
+        }
+        
+        await store.send(.appStart)
+        await store.receive(\.degreeChange) {
+            $0.appLogoDegreeChange = true
+        }
+        await store.receive(\.fetchOnboardingCompleted) {
+            $0.isOnboardingCompleted = false
+        }
+        await store.receive(\.quitSplash) {
+            $0.isAppStarting = false
+        }
+    }
+    
+    // 온보딩 완료 버튼 테스트
+    func testOnboardingStartButton() async {
+        var isOnboardingCompleted: Bool = false
+        let commons = Commons(isCompleteOnboarding: { isOnboardingCompleted },
+                              completeOnboarding: { isOnboardingCompleted = true })
+        
+        let store = TestStore(initialState: AppStartFeature.State()) {
+            AppStartFeature()
+        } withDependencies: {
+            $0.commons = commons
+        }
+        
+        await store.send(.startButtonTapped)
+        await store.receive(\.completeOnboarding)
+        await store.receive(\.refetchCompleteOnboarding) {
+            $0.isOnboardingCompleted = true
+        }
+    }
     
 }
