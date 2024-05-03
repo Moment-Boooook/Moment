97d50f7cd521b41ee4799fb5aa58bb3efc1f7837
Phangg <leeckdwns9305@gmail.com>
2024-05-04 00:04:14 +0900

    [Add] NameSettingView 작성
    초기 사용자에게 이름 작성하게 하는 화면 작성
    - 최대 12자

---
+++ Moment_TCA_1.9.2/Moment/Moment/Splash + Onboarding + NameSetting/NameSettingView.swift
@@ -0,0 +1,79 @@
+//
+//  NameSettingView.swift
+//  Moment
+//
+//  Created by phang on 5/3/24.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+// MARK: - Name Setting View
+struct NameSettingView: View {
+    @Bindable var store: StoreOf<AppStartFeature>
+    @FocusState var focusedField: Bool
+
+    var body: some View {
+        VStack(alignment: .center) {
+            // Lottie 애니메이션 뷰
+            LottieView(jsonName: "book")
+                .aspectRatio(1.0, contentMode: .fit)
+                .frame(width: 140)
+            // 이름 등록 안내 텍스트
+            VStack(alignment: .center, spacing: 6) {
+                Text("사용할 이름을 등록해주세요!")
+                    .font(.medium24)
+                // TODO: - 프로필 및 설정 페이지 만든 뒤..
+//                Text("이름은 언제든 변경할 수 있어요")
+//                    .font(.regular16)
+            }
+            .padding(.vertical, 20)
+            // 이름 textfield
+            TextField("이름 설정",
+                      text: $store.name.sending(\.setName),
+                      prompt: Text("\(store.maxLength)자 이내").font(.regular16).foregroundStyle(.gray2))
+            .focused($focusedField)
+            .multilineTextAlignment(.center)
+            .textInputAutocapitalization(.never)
+            .autocorrectionDisabled(true)
+            .onChange(of: store.name) { oldValue, newValue in
+                if newValue.count > store.maxLength {
+                    store.send(.setName(oldValue))
+                }
+            }
+            .textFieldStyle(
+                .bordered(color: store.name.isEmpty ? .gray2 : .lightBrown)
+            )
+            .foregroundStyle(.black)
+            .frame(height: 20)
+            .padding(.vertical, 10)
+            .padding(.bottom, 20)
+            // 저장 버튼
+            Button {
+                store.send(.saveName)
+            } label: {
+                Text("저장")
+                    .font(.medium16)
+                    .foregroundStyle(.white)
+            }
+            .disabled(store.name.isEmpty)
+            .buttonStyle(.capsuled(color: store.name.isEmpty ? .gray3 : .mainBrown,
+                                   width: 100))
+        }
+        .bind($store.focusedField, to: $focusedField)
+        .padding(.horizontal, 20)
+    }
+}
+
+#Preview {
+    NameSettingView(
+        store: Store(
+            initialState: AppStartFeature.State(books: Shared([]),
+                                                records: Shared([]),
+                                                currentOnboardingPage: .first)
+        ) {
+            AppStartFeature()
+        }
+    )
+}
